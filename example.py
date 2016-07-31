7b307e4c6d9027e19e93fd8cbf8f5732365a6df3
David Heyman <d.heyman256@gmail.com>
2016-07-31 14:57:41 -0400

    Slight clarity improvement to example

--- example.py
+++ example.py
@@ -8,8 +8,15 @@ namelist = ['Sarah', 'Ellen', 'Frederick', 'Robert the Seventeenth',
 
 paired = [(n, n[0]) for n in namelist]
 
-gen = format_generator('{0} is a{2} name, and it starts with {1}.',
-                       paired, [' good', 'n excellent'])
+template = '{0} is a{2} name, and it starts with {1}.'
+
+gen = format_generator(template, paired, [' good', 'n excellent'])
+
+print "Template is:"
+print template
+
+print "Args are:"
+print (paired, [' good', 'n excellent'])
 
 for sentence in gen:
     print sentence
